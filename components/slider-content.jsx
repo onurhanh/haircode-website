20053833c60e219c5cc15abbc12248463e94eabe
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-05-25 16:41:33 +0300

    fix: text

--- components/slider-content.jsx
+++ components/slider-content.jsx
@@ -9,7 +9,7 @@ export default function SliderContent() {
 
       {/* Yukarıdan inen başlık */}
       <motion.h2
-        className="text-6xl font-bold tracking-widest"
+        className="text-3xl sm:text-6xl font-bold tracking-widest"
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
