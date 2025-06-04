979a572b316f2e21f37607d25ce35cc39e4db7fa
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-06-04 20:11:45 +0300

    fix:import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,8 @@
 
 import About from "../../components/about";
 import Contact from "../../components/contact";
-import Directions from "../../components/directions";
 import Footer from "../../components/footer";
 import GoogleReviewsEmbed from "../../components/googleComment";
-import InstagramFeed from "../../components/instagram";
 import Services from "../../components/services";
 import Slider from "../../components/slider";
 import Waicon from "../../components/waicon";
