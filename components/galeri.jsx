7b6d2d3583b95eb30dab46ee2246120f0ca93c62
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-06-12 01:42:56 +0300

    fix:gallery

--- components/galeri.jsx
+++ components/galeri.jsx
@@ -7,7 +7,7 @@ export default function InstagramFeed() {
   return (
     <section id="galeri" className="bg-[#E5E2E2] py-10 px-6">
       <h2 className="md:text-6xl text-4xl font-sans text-center mt-15 mb-15">Galeri</h2>
-      <div className="max-w-6xl mx-auto ">
+      <div className="max-w-6xl mx-auto mb-10 md:mb-20">
         <script src="https://static.elfsight.com/platform/platform.js" async></script>
         <div class="elfsight-app-4cfe4e9f-1b31-4b5c-8c10-1103c91dfff4" data-elfsight-app-lazy></div>
       </div>
