466eb313af53b339441448c92d138dccfa5acd97
onurhan <onurhanhacimustafaoglu@gmail.com>
2025-06-11 17:51:42 +0300

    feat:instagram

--- components/galeri.jsx
+++ components/galeri.jsx
@@ -2,28 +2,14 @@
 
 import React from "react";
 
-const instagramImages = [
-  "https://www.instagram.com/p/DKMRpThIonm/",
-  "https://www.instagram.com/p/DKSUgUvsXaL/",
-  "https://www.instagram.com/p/DJ9Yh6mIGlc/",
-];
 
 export default function InstagramFeed() {
   return (
-    <section id="galeri" className="bg-[#f8f4ee] py-10 px-6">
-      <h2 className="text-2xl font-semibold text-center mb-6">Instagram'da Biz</h2>
-      <div className="max-w-5xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-        {instagramImages.map((src, i) => (
-          <a
-            key={i}
-            href="https://instagram.com/haircode"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:opacity-80 transition duration-300"
-          >
-            <img src={src} alt={`Post ${i + 1}`} className="rounded-lg w-full h-auto" />
-          </a>
-        ))}
+    <section id="galeri" className="bg-[#E5E2E2] py-10 px-6">
+      <h2 className="md:text-6xl text-4xl font-sans text-center mt-15 mb-15">Galeri</h2>
+      <div className="max-w-6xl mx-auto ">
+        <script src="https://static.elfsight.com/platform/platform.js" async></script>
+        <div class="elfsight-app-4cfe4e9f-1b31-4b5c-8c10-1103c91dfff4" data-elfsight-app-lazy></div>
       </div>
     </section>
   );
